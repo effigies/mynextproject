2df632380ab99fd1e24a2c0f0efd11ca27e13229
Chris Markiewicz <effigies@gmail.com>
2024-10-21 14:28:28 -0400

    Archiving all my old work

---
+++ src/mynextproject/archive/mytool.py
@@ -0,0 +1,14 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "nibabel",
+# ]
+# ///
+
+
+def main() -> None:
+    print("Hello from mytool.py!")
+
+
+if __name__ == "__main__":
+    main()
